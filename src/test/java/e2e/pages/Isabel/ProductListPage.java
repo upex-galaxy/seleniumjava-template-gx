a87fd4915405faa0a4f4491ddd582115771eecd1
igflores <139146133+igflores@users.noreply.github.com>
2023-11-08 09:58:39 -0800

    Test(GX3-2910): Added my POM of Login, starting the POM of View shopping cart items and PLP

---
+++ src/test/java/e2e/pages/Isabel/ProductListPage.java
@@ -0,0 +1,54 @@
+package e2e.pages.Isabel;
+
+import java.util.List;
+import java.util.function.Supplier;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import e2e.utils.Locator;
+
+public abstract class ProductListPage {
+    private Locator get;
+    private Supplier<List<WebElement>> productItems;
+    private Supplier<List<WebElement>> productPrices;
+    private Supplier<List<WebElement>> addToCartButton;
+    private Supplier<List<WebElement>> removeButton;
+
+    public ProductListPage(WebDriver driver, Locator locator) {
+        this.get = locator;
+        this.productItems = () -> this.get.ByClasses("inventory_item");
+        this.productPrices = () -> this.get.ByClasses("inventory_item_price");
+        this.addToCartButton = () -> this.get.Selectors("[data-test*=add-to-cart]");
+        this.removeButton = () -> this.get.Selectors("[data-test*=remove]");
+    }
+
+    // ----------- GETTERS ---------------------------------------------
+    public WebElement getProductItem(Integer productIndex) {
+        WebElement item = this.productItems.get().get(productIndex);
+        return item;
+    }
+
+    public String getProductPrice(Integer productIndex) {
+        WebElement price = this.productPrices.get().get(productIndex);
+        String priceValue = price.getText();
+        return priceValue;
+    }
+
+    // ----------- MAIN ACTIONS ---------------------------------------------
+    public void addProductToCart(Integer productIndex) {
+        WebElement button = this.addToCartButton.get().get(productIndex);
+        button.click();
+    }
+
+    public void removeProductFromCart(Integer productIndex) {
+        WebElement button = this.removeButton.get().get(productIndex);
+        button.click();
+    }
+
+    // ----------- Navigators ---------------------------------------------
+    public void gotoProductDetails(WebElement givenProduct) {
+        WebElement item = this.get.WithinElement(givenProduct, "img");
+        item.click();
+    }
+
+    // seleccionarProducto(){ obtener el elemento del producto}
+}
--- src/test/java/e2e/pages/Isabel/ViewShoppingCartItemsPage.java
+++ src/test/java/e2e/pages/Isabel/ViewShoppingCartItemsPage.java
@@ -4,30 +4,24 @@ import java.util.function.Supplier;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import e2e.utils.Action;
-import e2e.utils.Assertion;
 import e2e.utils.Locator;
 
 public class ViewShoppingCartItemsPage {
     WebDriver web;
     Locator get;
     Action Do;
-    Assertion validate;
+
     private Supplier<WebElement> cancelButton;
     private Supplier<WebElement> finishButton;
 
-    // 1. ARMAR EL CONSTRUCTOR con WebDriver (para usar los selectores/Locators)
     public ViewShoppingCartItemsPage(WebDriver driver, Locator locator, Action action) {
         this.web = driver;
         this.get = locator;
         this.Do = action;
-        this.validate = new Assertion();
-
-        // 2. LOCATORS DE PAGE => dentro del Constructor (requiere tipado Suppiler<>)
         this.cancelButton = () -> this.get.ByTestId("cancel");
         this.finishButton = () -> this.get.ByTestId("finish");
     }
 
-    // 3. MÉTODOS CON LOS SELECTORES => métodos public
     public void cancelOrder() {
         this.Do.click(this.cancelButton.get());
     }
